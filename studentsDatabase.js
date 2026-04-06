ca4a2eb599163537329de6f14031edad196fe99d
Awallence <awallencecode@gmail.com>
2026-04-06 23:44:01 +0300

    Add student creation functionality and main function to studentsDatabase.js

--- studentsDatabase.js
+++ studentsDatabase.js
@@ -1,1 +1,36 @@
 const prompt = require("prompt-sync")(); 
+
+//object for create students
+createStudent(name, age)
+{
+    return {
+
+        addGrade: function()
+        {
+
+        },
+
+        getAverage: function()
+        {
+
+        },
+
+        getBestSubject: function()
+        {
+            
+        },
+
+        getReport: function()
+        {
+
+        }
+    }
+}
+
+//main function 
+function main()
+{
+
+}
+
+main();
